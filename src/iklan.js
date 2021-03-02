d24311d9c19de0ca6275a779fe2c93273cca2ce0
Zanxx <78849151+akmalcoeg19@users.noreply.github.com>
2021-03-02 17:39:23 +0700

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,10 @@
+const iklan1 = (pushname, prefix, botName, ownerName) => { 
+	return `🔰 -----[ *「 IKLAN ${botName} 」* ]----- 🔰
+──────────────────────────────
+◯ *PRENKKK INI KAMERANYA :*
+──────────────────────────────
+🔰 -----[ *「 POWERED BY ${ownerName} 」* ]----- 🔰
+`
+}
+
+exports.iklan1 = iklan1
